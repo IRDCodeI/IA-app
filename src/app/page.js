a7dc6ca992fe5c5616134a2f72878d3f7e82c69e
RDCode <rkostalin@gmail.com>
2023-05-26 11:06:25 -0500

    started socket implementation

--- src/app/page.js
+++ src/app/page.js
@@ -1,56 +1,47 @@
-import { parse } from 'csv-parse';
-import UploadFile from '@/components/upload';
+import DataFields from "@/components/fields";
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center p-24 static">
-      <div className='w-100 h-auto'>
+      <div className="w-100 h-auto">
         <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">IA</span> Application</h1>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            IA
+          </span>
+          Application
+        </h1>
       </div>
       <div className="w-11/12 mx-10 relative flex py-5 items-center">
         <div className="flex-grow border-t border-white"></div>
         <span className="flex-shrink mx-4 text-gray-400">File</span>
         <div className="flex-grow border-t border-white"></div>
       </div>
-      <form method='post'>
-        <div className='w-max relative'>
-          <div className="w-auto h-auto self-center
+      <form method="post">
+        <div className="w-max relative">
+          <div
+            className="w-auto h-auto self-center
             before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] 
             after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] 
-            before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-          </div>
-          <UploadFile></UploadFile>
+            before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"
+          ></div>
+          <DataFields></DataFields>
         </div>
-        <div className='absolute bottom-10 right-10'>
-          <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Send File</button>
-          <button type="reset" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete File</button>
+        <div className="absolute bottom-10 right-10">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            Send File
+          </button>
+          <button
+            type="reset"
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            Delete File
+          </button>
         </div>
       </form>
-      <div className="w-11/12 mx-10 relative flex flex-col py-5 items-center">
-        <span className="flex-shrink mx-4 my-5 text-xl font-semibold text-gray-400">Dataset Information</span>
-        <div className="grid grid-cols-6 gap-4">
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Atribute</h5>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Atribute</h5>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Atribute</h5>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Atribute</h5>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Atribute</h5>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Atribute</h5>
-          </div>
-        </div>
-      </div>
+      
     </main>
-  )
+  );
 }
--- src/components/upload.js
+++ src/components/upload.js
@@ -1,50 +1,56 @@
-'use client'
-import { Button } from '@mui/material';
-import { useRef } from 'react';
-import { parse } from 'csv-parse';
+"use client";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
 
-export default function UploadFile() {
+export default function UploadFile({ dataFields }) {
+  const [fileName, setFileName] = useState();
+  const [file, setFile] = useState();
 
-    let file = null
-    let csv = null
+  useEffect(() => {
+    if(file){
+      const reader = new FileReader();
 
-    const inputFile = useRef(null);
-    const fileName = useRef(null)
+      reader.onload = function (e) {
+        Papa.parse(e.target.result, {
+          delimiter: ",",
+          header: true,
+          complete: (res) => {
+            dataFields(res.meta.fields)
+          }
+        })
+      };
 
-    const readFile = () => {
-        const reader = new FileReader()
-        file = inputFile.current.files[0]
-
-        reader.onload = () => {
-            fileName.current.innerText = file.name
-            csv = reader.result  
-        }
-
-        reader.readAsBinaryString(file)            
+      reader.readAsText(file);
     }
+  }, [dataFields, file]);
 
-    return (
-        <div className='w-screen flex'>            
-            <div className='w-screen mx-14 flex justify-center items-center'>
-                <Button
-                    type="button"
-                    className="px-5 py-2.5 inline-flex items-center justify-center overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-                    variant='contained'
-                    component="label"
-                    >
-                    Upload File
-                    
-                    <input
-                        className='px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'
-                        type="file"
-                        ref={inputFile}
-                        accept='.csv'
-                        onChange={readFile}
-                        style={{ display: 'none' }} />
-                </Button>
-                <span className='mx-4 w-64 max-w-lg text-l font-semibold leading-normal text-gray-900 dark:text-white'
-                 ref={fileName}></span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-screen flex">
+      <div className="w-screen mx-14 flex justify-center items-center">
+        <Button
+          type="button"
+          className="px-5 py-2.5 font-semibold inline-flex items-center justify-center overflow-hidden text-sm text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+          variant="contained"
+          component="label"
+        >
+          Upload File
+          <input
+            className="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+            type="file"
+            name="csvFile"
+            accept=".csv"
+            onChange={(e) => {
+              setFile(e.target.files[0]);
+              setFileName(e.target.files[0].name);
+            }}
+            style={{ display: "none" }}
+          />
+        </Button>
+        <span className="mx-4 w-64 max-w-lg text-l font-semibold leading-normal text-gray-900 dark:text-white">
+          {fileName}
+        </span>
+      </div>
+    </div>
+  );
 }
